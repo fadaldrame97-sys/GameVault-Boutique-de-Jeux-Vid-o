63b08cf456aecc1abbd3534443da8238ba46fec5
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-04-02 10:48:06 +0100

    Créer un H2 et P pour afficher le tiite du jeu et le prix en DH

--- panier.js
+++ panier.js
@@ -2,13 +2,14 @@ const article=JSON.parse(localStorage.getItem('cart'))||[];
 
 const contenue=document.getElementById("panier-artcl");
 
-cart.forEach(element => {
+cart.forEach(game => {
 
     const div=document.createElement('div');
 
     div.innerHTML=`
     
-    
+    <h2>${game.tilte}</h2>
+    <h2>${game.price}</h2>
     
     
     
