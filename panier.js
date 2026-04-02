ae7e14381d2239a11d7a7e6546d525786f7c5f0a
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-04-02 12:04:27 +0100

    j'ai ajouté le button + et - pour que l'utilisateur puisse ajouter plusieurs quantités de son article

--- panier.js
+++ panier.js
@@ -2,14 +2,28 @@ const article=JSON.parse(localStorage.getItem('cart'))||[];
 
 const contenue=document.getElementById("panier-artcl");
 
-cart.forEach(game => {
+article.forEach(game => {
 
     const div=document.createElement('div');
-
+    div.className = "border p-4 m-2 rounded";
+  div.className ="flex flex-col  jsutify-center items-center gap-4 border p-4 m-2 rounded shadow";
     div.innerHTML=`
-    
-    <h2>${game.tilte}</h2>
+      <img " src="${game.image}"
+           
+    <h2>${game.title}</h2>
     <h2>${game.price}</h2>
+   
+      <div class=" flex flex-row gap-10">
+        <h2>-</h2>
+        <h2>1</h2> 
+    <h2>+</h2>
+      </div>
+
+      <div class=" flex flex-row gap-10"> 
+      
+      
+      
+      </div>
     
     
     
